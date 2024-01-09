3ed4a187af00567d07cdea2ad1f2aa81db856de8
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-09 08:57:22 +0545

    Add gltf package and update camera position.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
+import { Dragon } from "./Dragon";
+import { AmbientLight } from "three";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+        {" "}
+        <Dragon />
+      </group>
+
+      <ambientLight intensity={1} />
     </>
   );
 };
