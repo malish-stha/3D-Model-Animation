6b88a74f1f832ba66e61dd37b98d304ec17fa2b6
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-10 10:08:04 +0545

    Add gsap library and update menu component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,38 @@ import { Experience } from "./components/Experience";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import { Interface } from "./components/Interface";
 import Menu from "./components/Menu";
+import { useEffect, useState } from "react";
+import { Leva } from "leva";
+import { ScrollManager } from "./components/ScrollManager";
 
 function App() {
+  const [section, setSection] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  useEffect(() => {
+    setMenuOpened(false);
+  }, [section]);
+
   return (
     <>
       <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
         <color attach="background" args={["#ececec"]} />
         <ScrollControls pages={4} damping={0.1}>
-          <Experience />
+          <ScrollManager section={section} onSectionChange={setSection} />
+
+          <Experience section={section} />
+
           <Scroll html>
             <Interface />
           </Scroll>
         </ScrollControls>
       </Canvas>
-      <Menu onSectionChange={setSection} />
+      <Menu
+        onSectionChange={setSection}
+        menuOpened={menuOpened}
+        setMenuOpened={setMenuOpened}
+      />
+      <Leva hidden />
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,24 +1,41 @@
-import { OrbitControls } from "@react-three/drei";
 import { Dragon } from "./Dragon";
 import { useControls } from "leva";
+import { motion } from "framer-motion-3d";
+import { useThree } from "@react-three/fiber";
 
-export const Experience = () => {
+export const Experience = (props) => {
+  const { section } = props;
   const { animation } = useControls({
     animation: {
       value: "run",
-      options: ["run", "skill01", "skill02"],
+      options: ["run", "skill01", "skill02", "atk01", "deaddown"],
     },
   });
 
   return (
     <>
-      {/* <OrbitControls /> */}
-
-      <group position-y={-1}>
-        <Dragon animation={animation} />
-      </group>
-
       <ambientLight intensity={1} />
+      <motion.group>
+        <motion.group
+          position={[0.1, 0.1, 2]}
+          scale={[0.9, 0.9, 0.9]}
+          rotation-y={-Math.PI / 4}
+        >
+          <Dragon
+            animation={
+              section === 0
+                ? "run"
+                : section === 1
+                ? "skill02"
+                : section === 2
+                ? "atk01"
+                : section === 3
+                ? "deaddown"
+                : "run"
+            }
+          />
+        </motion.group>
+      </motion.group>
     </>
   );
 };
